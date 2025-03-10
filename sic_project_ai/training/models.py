8ba5c6a6a189afe72bb339ccc1fd9c0fbc635ee5
Ezzz-Lui <ogn.lui@gmail.com>
2025-03-09 21:32:50 -0600

    Add AiModel class to define AI model attributes and metadata

--- sic_project_ai/training/models.py
+++ sic_project_ai/training/models.py
@@ -9,3 +9,12 @@ class agent_model(models.Model):
     agent_created = models.DateTimeField(auto_now_add=True)
     agent_updated = models.DateTimeField(auto_now=True)
     agent_lore = models.TextField(max_length=max)
+
+
+class AiModel(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=50)
+    model_type=models.CharField(max_length=50)
+    framework=models.CharField(max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
