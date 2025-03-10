8b1c823ccc5f8580907277f804ef007b7bb1c0b1
Ezzz-Lui <ogn.lui@gmail.com>
2025-03-09 21:32:58 -0600

    Add URL patterns for AiModels and update Agents URLs for consistency

--- sic_project_ai/training/urls.py
+++ sic_project_ai/training/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import AgentsView
+from .views import AgentsView, AiModelsView
 
 class AgentsUrlsPatterns:
     urlPatterns = [
-        path('services/agents/list/', AgentsView.get_all, name='get_all_agents'),
-        path('services/agents/create/', AgentsView.create_agent, name='create_agent'),
-        path('services/agents/update/<int:id>/', AgentsView.update_agent, name='update_agent'),
-        path('services/agents/delete/<int:id>/', AgentsView.delete_agent, name='delete_agent'),
+        path('actions-agent/list', AgentsView.get_all, name='get_all_agents'),
+        path('actions-agent/create', AgentsView.create_agent, name='create_agent'),
+        path('actions-agent/update/<int:pk>/', AgentsView.update_agent, name='update_agent'),
+        path('actions-agent/delete/<int:pk>/', AgentsView.delete_agent, name='delete_agent'),
     ]
+
+class ModelsUrlPatterns:
+    urlpatterns = [
+        path('actions-models/list', AiModelsView.get_all, name='get_all_models'),
+        path('actions-models/create', AiModelsView.create_ai_model, name='create_model'),
+        path('actions-models/update/<int:pk>/', AiModelsView.update_ai_model, name='update_model'),
+        path('actions-models/delete/<int:pk>/', AiModelsView.delete_ai_model, name='delete_model'),
+    ]
+    
