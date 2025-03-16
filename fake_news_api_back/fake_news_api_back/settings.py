40ff4fd5e5c1a26c76c11950cc743440afda4b15
Ezzz-Lui <ogn.lui@gmail.com>
2025-03-16 04:53:12 -0600

    feat: restrict CORS to allow only specified origins for enhanced security

--- fake_news_api_back/fake_news_api_back/settings.py
+++ fake_news_api_back/fake_news_api_back/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", 
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
